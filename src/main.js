3efa6c16b30bf9335360b50065e119200fdb0033
zhangxiaoyan <413489529@qq.com>
2018-05-11 13:27:31 +0800

    change z-index

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 var app = new Vue({
     el:"#app",
     data:{
-        loading:true,
-        loadingMessage:`Hi. I am Zhang Xiaoyan. Welcome to  my site`,
-        message:'My page is building. Please wait...'
+        message:'Hi. I am Zhang Xiaoyan. Welcome to my site.'
     },
     beforeMount:function(){
         
     },
     mounted:function(){
-        this.loading=false;
+        this.message="Everything is coming soon..."
     }
 })
