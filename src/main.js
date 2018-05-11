79ac5788b4d2721ff89d44f78a6ca40e6ee29f38
zhangxiaoyan <413489529@qq.com>
2018-05-11 13:04:52 +0800

    add loading

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 var app = new Vue({
     el:"#app",
     data:{
-        message:'Hello everyone. My page is building. Please wait...'
+        loading:true,
+        loadingMessage:`Hi. I am Zhang Xiaoyan. Welcome to  my site`,
+        message:'My page is building. Please wait...'
+    },
+    beforeMount:function(){
+        
+    },
+    mounted:function(){
+        this.loading=false;
     }
 })
